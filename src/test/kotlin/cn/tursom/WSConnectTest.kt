621ee195d2633cda2fc61d1b591d618dff50d306
tursom <tursom@foxmail.com>
2021-07-08 21:36:39 +0800

    add protobuf struct danmu data

---
+++ src/test/kotlin/cn/tursom/WSConnectTest.kt
@@ -0,0 +1,25 @@
+package cn.tursom
+
+import cn.tursom.core.notifyAll
+import cn.tursom.core.wait
+import  cn.tursom.ws.BiliWSClient
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class WSConnectTest {
+  @Test
+  fun testConnect(): Unit = runBlocking {
+    val biliWSClient = BiliWSClient(3044248, onClose = {
+      notifyAll {
+      }
+    })
+    biliWSClient.addDanmuListener {
+      println(it)
+    }
+
+    biliWSClient.connect()
+    biliWSClient.wait {
+    }
+  }
+
+}
